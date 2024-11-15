8dfc63e747588f521a0da4e108338aaf6540c092
Joaquin <166671976+SaliJz@users.noreply.github.com>
2024-11-15 16:19:05 -0500

    Se añadieron las escenas de Derrota por Vida, Derrota por Cordura, Victoria con Tumi y Victoria sin Tumi, junto con los códigos necesarios para gestionar los cambios de escenario, incluyendo la transición a los créditos y la carga de la última escena guardada en caso de derrota. Además, se incorporaron los modelos del crucifijo y la escopeta, junto con sus texturas aplicadas.

---
+++ Assets/Scripts/UI/RetryButtonChangeScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RetryButtonChangeScene : MonoBehaviour
+{
+    public static string SCENE_NAME = string.Empty;
+    [SerializeField] private AudioSource fxSource;
+    [SerializeField] private AudioClip clickSound;
+
+    void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene()
+    {
+        PlaySoundButton();
+        if (SCENE_NAME == string.Empty)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(SCENE_NAME);
+        }
+    }
+
+    private void PlaySoundButton()
+    {
+        fxSource.PlayOneShot(clickSound);
+    }
+}
