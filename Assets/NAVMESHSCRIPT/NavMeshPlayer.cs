069a2bc4a57e311cce56b422f9e39fb7f924f92d
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-11-22 13:26:01 -0500

    Agregando cinmachine y movimiento por medio del mouse

--- Assets/NAVMESHSCRIPT/NavMeshPlayer.cs
+++ Assets/NAVMESHSCRIPT/NavMeshPlayer.cs
@@ -2,19 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Cinemachine;
 
 public class NavMeshPlayer : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public GameObject bolaDestino;
+    private Camera cinemachineCamera;
+
     void Start()
     {
-        navMeshAgent.destination = bolaDestino.transform.position;
+        CinemachineBrain brain = FindObjectOfType<CinemachineBrain>();
+
+        if (brain != null && brain.OutputCamera != null)
+        {
+            cinemachineCamera = brain.OutputCamera;
+        }
+
+        else
+        {
+            Debug.LogError("No se encontró una CinemachineBrain o una cámara activa.");
+        }
+
+        if (bolaDestino != null)
+        {
+            navMeshAgent.destination = bolaDestino.transform.position;
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado 'bolaDestino'.");
+        }
     }
 
    
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && cinemachineCamera != null)
+        {
+            Ray ray = cinemachineCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.destination = hit.point; 
+            }
+        }
     }
 }
